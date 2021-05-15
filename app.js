d2262713c96ceae27987f084837037c2a0b113a6
Tiffany <Owhyze1@gmail.com>
2021-05-14 21:31:57 -0400

    Onclick function appends new tweets to Home Feed

--- app.js
+++ app.js
@@ -16,19 +16,36 @@ $(document).ready(function(){
   $HomeFeed.appendTo($TweetFeed);
 
   var $updateFeedButton = $('<button type="button" id="update-feed">Update Feed</button>');
-  $updateFeedButton.appendTo($TweetFeed);
+  $updateFeedButton.appendTo($HomeFeed);
   $updateFeedButton.on("click", function(event) {
-    console.log(event);
-    alert('Feed updating');
+
+    // need to update tweets
+    var streamSize = streams.home.length - 1;
+    addNewTweetsForUpdateFeedButton(lastStreamSize);
   });
 
+
   var index = streams.home.length - 1;
+  var lastStreamSize = index;
   while(index >= 0){
     var tweet = streams.home[index];
     var $tweet = $('<div class="tweet"></div>');
     $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($TweetFeed);
+    $tweet.appendTo($HomeFeed);
     index -= 1;
+  }
+
+  var addNewTweetsForUpdateFeedButton = function(index) {
+    var sizeOfStreamBeforeUpdate = index;
+    var currentStreamSize = streams.home.length - 1;
+
+    while (currentStreamSize > sizeOfStreamBeforeUpdate){
+      var tweet = streams.home[currentStreamSize];
+      var $tweet = $('<div class="tweet"></div>');
+      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      $tweet.appendTo($HomeFeed);
+      currentStreamSize -= 1;
+    }
   }
 
 });
