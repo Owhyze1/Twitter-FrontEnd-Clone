f741ac154a9c2f592eea5f08a87437aab7c5fa4b
Tiffany <Owhyze1@gmail.com>
2021-05-14 20:56:38 -0400

    Added TweetFeed and HomeFeed divs and an update-feed button

--- app.js
+++ app.js
@@ -8,12 +8,26 @@ $(document).ready(function(){
     console.log(event);
     alert('The title of this page is: ' + event.target.innerText);
   });
+
+  var $TweetFeed = $('<div><h2>Tweet Feed</h2></div>');
+  $TweetFeed.appendTo($app);
+
+  var $HomeFeed = $('<div><h3>Home Feed</h3></div>');
+  $HomeFeed.appendTo($TweetFeed);
+
+  var $updateFeedButton = $('<button type="button" id="update-feed">Update Feed</button>');
+  $updateFeedButton.appendTo($TweetFeed);
+  $updateFeedButton.on("click", function(event) {
+    console.log(event);
+    alert('Feed updating');
+  });
+
   var index = streams.home.length - 1;
   while(index >= 0){
     var tweet = streams.home[index];
     var $tweet = $('<div class="tweet"></div>');
     $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($app);
+    $tweet.appendTo($TweetFeed);
     index -= 1;
   }
 
