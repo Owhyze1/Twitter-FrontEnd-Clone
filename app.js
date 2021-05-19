d308c6d677f591d04052baeb14529f923c10be70
Tiffany <Owhyze1@gmail.com>
2021-05-18 22:57:31 -0400

    Refactor. Add elements for Friends List.

--- app.js
+++ app.js
@@ -4,33 +4,45 @@ $(document).ready(function () {
 
   // select existing elements
   var $app = $('#app');
-  // $app.html('');
+
 
   // create new HTML elements
-  var $title = $('<h1>Twiddler</h1>');
-  var $TweetFeed = $('<div><h2>Tweet Feed</h2></div>');
-  var $HomeFeed = $('<div><h3>Home Feed</h3></div>');
+  var $title = $('<div class="title">Twiddler</div>');
   var $updateFeedButton = $('<button type="button" id="update-feed">Update Feed</button>');
+  var $bottomHalf = $('<div id="bottom-half"></div>');
+  var $leftMenu = $('<div id="left-half"></div>');
   var $feed = $('<div id="feed"></div>');
-
-  var $UserFeed = $('<div><h3>User Feed</h3></div');
-
+  var $friendList = $('<div id="friend-list">Friends</div>');
 
   // create event handler functions
   function titleEvent(event) {
-    console.log(event);
-    alert('The title of this page is: ' + event.target.innerText);
   }
   function updateFeedEvent(event) {
-    renderFeed(numberOfDisplayedTweets);
+    $feed.html('');
+    renderFeed();
   }
   function iconMouseOverColorChange(event) {
     $(event.target).css("color", "tomato");
+    $(event.target).addClass("fa-lg");
   }
   function iconMouseOutColorReturn(event) {
     $(event.target).css("color", "black");
+    $(event.target).removeClass("fa-lg");
   }
+  function handleUsernameClick(event) {
+    // re-render feed with only user's tweets
+    $feed.html('');
+    var username = event.target.outerText.substring(1);
+    renderFeed(username);
 
+    // change updateFeed button to Back button, and back again
+    if ($updateFeedButton.text() === "Update Feed") {
+      $updateFeedButton.text("Back");
+    }
+    $updateFeedButton.on("click", function(event) {
+      $updateFeedButton.text("Update Feed");
+    });
+  }
 
   // set event listeners (and relevant handlers)
   $title.on("click", titleEvent);
@@ -39,60 +51,45 @@ $(document).ready(function () {
 
   // append new html elements to the DOM
   $title.appendTo($app);
-  $TweetFeed.appendTo($app);
-  $HomeFeed.appendTo($TweetFeed);
-  $updateFeedButton.appendTo($HomeFeed);
-  $feed.appendTo($HomeFeed);
+  $updateFeedButton.appendTo($title);
+  $bottomHalf.appendTo($app);
+  $leftMenu.appendTo($bottomHalf);
+  $feed.appendTo($bottomHalf);
+  $friendList.appendTo($leftMenu);
 
-  // var lastStreamSize = streams.home.length - 1;
-  var numberOfDisplayedTweets = renderFeed(0, "shawndrost");
+  renderFeed();
 
   // helper functions
-  function renderFeed(oldestTweet, user) {
-    if ( user === undefined ) {
-      var tweets = streams.home;
-      var feed = $feed;
-    } else {
-      var tweets = streams.users[user];
-      var feed = $feed;//$UserFeed;
-    }
+  function renderFeed(user) {
+    tweets = user === undefined ? streams.home : streams.users[user];
 
     var currentSreamSize = tweets.length - 1;
-    var lastestTweets = [];
+    var start = 0;
     var indexOfNewestTweet = currentSreamSize;
 
-    while (indexOfNewestTweet > oldestTweet) {
+    while (indexOfNewestTweet > 0) {
       var tweet = tweets[indexOfNewestTweet];
       var $tweet = tweetUIcomponent(tweet);
-      lastestTweets.push($tweet);
+      $tweet.appendTo($feed);
       indexOfNewestTweet -= 1;
     }
-    feed.prepend(lastestTweets);
-    return currentSreamSize;
   }
 
-  // function renderUserFeed(user){
-  //   var tweets = streams.users.user;
-  //   var index = tweets.length - 1;
-  //   var latestTweets = [];
-
-  //   while (index >= 0 ) {
-  //     var tweet = tweets[index];
-  //     var $tweet tweetUIcomponent(tweet);
-  //     latestTweets.push($tweet);
-  //     index -= 1;
-  //   }
-  //   $UserFeed.append(latestTweets);
-  //   // return
-  // }
-
+  function renderFriendList() {
+    var friends = Object.keys(streams.users);
+  }
   function tweetUIcomponent(tweet) {
     // new HTML elements
     var $tweet = $('<div class="tweet"></div>');
     var $img = $('<img class="profile-photo">');
-    var $username = $('<span class="username"></span>');
+
+    var $rightSideOfTweet = $('<div class="right-side"></div>');
+    var $username = $('<div class="username-div username"></div>');
     var $message = $('<span class="message"></span>');
+
+    var $timeAndIconDiv = $('<div class="time-and-icon-div"></div>');
     var $timestamp = $('<span class="timestamp"></span>');
+    var $iconDiv = $('<div class="icon-div"></div>');
     var $comment = $('<i class="icon comment far fa-comment"></i>');
     var $retweet = $('<i class="icon comment fas fa-retweet"></i>');
     var $like = $('<i class="icon comment far fa-heart"></i>');
@@ -118,16 +115,22 @@ $(document).ready(function () {
     $like.on("mouseout", iconMouseOutColorReturn);
     $share.on("mouseout", iconMouseOutColorReturn);
 
+    $username.on("click", handleUsernameClick);
+
 
     // append
     $img.appendTo($tweet);
-    $username.appendTo($tweet);
-    $message.appendTo($tweet);
-    $timestamp.appendTo($tweet);
-    $comment.appendTo($tweet);
-    $retweet.appendTo($tweet);
-    $like.appendTo($tweet);
-    $share.appendTo($tweet);
+    $rightSideOfTweet.appendTo($tweet);
+    $username.appendTo($rightSideOfTweet);
+    $message.appendTo($rightSideOfTweet);
+
+    $timeAndIconDiv.appendTo($rightSideOfTweet);
+    $timestamp.appendTo($timeAndIconDiv);
+    $iconDiv.appendTo($timeAndIconDiv);
+    $comment.appendTo($iconDiv);
+    $retweet.appendTo($iconDiv);
+    $like.appendTo($iconDiv);
+    $share.appendTo($iconDiv);
 
     return $tweet;
   }
